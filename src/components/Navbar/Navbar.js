0015fc5df493f71757e803affd913cd31a717614
Dina <>
2018-08-14 11:45:05 -0500

    logo fixed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,13 +2,14 @@
 // export default Navbar;
 import React from 'react';
 import "./Navbar.css";
+import logo from './logo.png';
 const Navbar = ({ score, topScore, message }) => {
 	return (
 		<div className='navbar-fixed'>
 			<nav>
 				<div className=' nav-wrapper row blue center-align flow-text navbar-fixed-top'>
 				<div className='col m4 s6' href="#">
-				<a href='/'><img src="/images/../../favicon.png" height="60" className="logo d-inline-block align-top"/></a>
+				<a href='/'><img src={logo} className="App-logo logo d-inline-block align-top" height="60" alt="logo" /></a>
                </div>
 					<div className='col m4 s6'>
 						<span className ="btn pulse orange accent-1 message">{message}</span>
